3d8ef04badcca429e69b018994db03b17094d772
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-11-25 19:57:38 +0600

    Lesson commit

---
+++ FixedDocumentLesson/FixedDocumentLesson/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace FixedDocumentLesson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadXpsClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var isProcessed = openFileDialog.ShowDialog();
+
+            if (isProcessed.Value)
+            {
+                var xpsFileName = openFileDialog.FileName;
+                using (XpsDocument xpsDocument = new XpsDocument(xpsFileName, FileAccess.ReadWrite))
+                {
+                    documentViewer.Document = xpsDocument.GetFixedDocumentSequence();
+                }
+            }
+        }
+
+        private void SaveXps(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SaveXps(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            var hasResult = saveFileDialog.ShowDialog();
+
+            if (hasResult.Value)
+            {
+                var xpsFileName = saveFileDialog.FileName;
+                using (var xpsDocument = new XpsDocument(xpsFileName, FileAccess.ReadWrite))
+                {
+                    XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+                    writer.Write(fixedDocument.DocumentPaginator);
+                }
+            }
+        }
+
+    }
+}
